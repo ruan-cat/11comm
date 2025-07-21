1d50f869f18205f831852f1346336ea42778a4cc
ruan-cat <1219043956@qq.com>
2025-07-22 05:38:21 +0800

    🐳 chore: 添加获取对应菜单组的接口及测试用例

--- apps/admin/src/api/j1/manu/manu.test.ts
+++ apps/admin/src/api/j1/manu/manu.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "vitest";
 import { printFormat } from "@ruan-cat/utils";
-import { addGroupCatalog, deleteGroupCatalog } from "./manu";
+import { addGroupCatalog, deleteGroupCatalog, queryGroupCatalog } from "./manu";
 
 describe("j1/菜单管理/菜单目录", () => {
 	it("使用 body 接口 - 添加对应菜单组", async () => {
@@ -39,6 +39,29 @@ describe("j1/菜单管理/菜单目录", () => {
 		await execute({
 			data: {
 				gcId: "102022022662880030",
+			},
+		});
+
+		console.warn("查看简单的 data.value ", printFormat(data.value));
+	});
+
+	it("使用 query 接口 - 获取对应菜单组", async () => {
+		const { execute, data } = queryGroupCatalog({
+			onSuccess(data) {
+				console.warn("queryGroupCatalog onSuccess", printFormat(data));
+			},
+			onError(error) {
+				console.error("queryGroupCatalog onError", error);
+			},
+		});
+
+		// 主动的做接口请求 从回调函数内获取返回值 或者直接使用解构出来的响应式 data 对象
+		await execute({
+			params: {
+				caId: "102022022695140008",
+				pageIndex: 1,
+				pageSize: 10,
+				storeType: "800900000000",
 			},
 		});
 
--- apps/admin/src/api/j1/manu/manu.ts
+++ apps/admin/src/api/j1/manu/manu.ts
@@ -30,6 +30,26 @@ export interface DeleteGroupCatalogResponse {
 	gcId: string;
 }
 
+/** 获取对应菜单组请求参数 */
+export interface QueryGroupCatalogParams {
+	/** 菜单目录编号 */
+	caId: string;
+	/** 查询页码 */
+	pageIndex: number;
+	/** 查询条数 */
+	pageSize: number;
+	/** 商户类型 */
+	storeType: string;
+}
+
+/** 获取对应菜单组响应参数 */
+export interface QueryGroupCatalogResponse {
+	/** 菜单组目录编号 */
+	gcId: string;
+	/** 菜单组编号 */
+	gid: string;
+}
+
 /**
  * 添加对应菜单组
  * @description 添加对应菜单组
@@ -63,6 +83,27 @@ export function deleteGroupCatalog<T = DeleteGroupCatalogResponse>(options?: Use
 			method: "DELETE",
 			data: {
 				gcId: "",
+			},
+		},
+	});
+}
+
+/**
+ * 获取对应菜单组
+ * @description 获取对应菜单组列表(条件+分页)
+ */
+export function queryGroupCatalog<T = PageDTO<QueryGroupCatalogResponse>>(options?: UseAxiosOptionsJsonVO<T>) {
+	return useRequest<ParamsQueryKey, T, QueryGroupCatalogParams>({
+		url: "/j1-manumana/groupCatalog/query",
+		httpParamWay: "query",
+		options,
+		config: {
+			method: "GET",
+			params: {
+				caId: "",
+				pageIndex: 1,
+				pageSize: 10,
+				storeType: "",
 			},
 		},
 	});
