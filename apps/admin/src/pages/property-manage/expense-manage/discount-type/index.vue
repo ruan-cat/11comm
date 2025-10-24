4e94d6875208db2d058dc373cbfc70e5920f4900
ruan-cat <1219043956@qq.com>
2025-10-24 22:27:06 +0800

    🐞 fix(admin)!: 处理 优惠类型 的语法bug。

--- apps/admin/src/pages/property-manage/expense-manage/discount-type/index.vue
+++ apps/admin/src/pages/property-manage/expense-manage/discount-type/index.vue
@@ -322,7 +322,7 @@ onMounted(async () => {
 			@reset="handleReSearch"
 		/>
 
-		<PureTableBar :=="pureTableBarProps" @refresh="handleReSearch">
+		<PureTableBar :="pureTableBarProps" @refresh="handleReSearch">
 			<template #buttons>
 				<ElButton type="primary" @click="openDialog({ mode: 'add' })">
 					{{ transformI18n($t("common.buttons.add")) }}
