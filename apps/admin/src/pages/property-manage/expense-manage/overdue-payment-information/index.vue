a985f61747ce1a8f70644029283f6ad81f633316
ruan-cat <1219043956@qq.com>
2025-10-25 15:44:06 +0800

    🦄 refactor(admin)!: 重构 欠费信息 列表页。

--- apps/admin/src/pages/property-manage/expense-manage/overdue-payment-information/index.vue
+++ apps/admin/src/pages/property-manage/expense-manage/overdue-payment-information/index.vue
@@ -8,81 +8,133 @@ definePage({
 	},
 });
 
-import { ref, computed, watch } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { transformI18n } from "@/plugins/i18n";
+import {
+	type 欠费信息_列表数据,
+	type 欠费信息_列表查询_VO,
+	收费对象Options,
+	tableData as mockTableData,
+} from "./test-data";
 
-interface 欠费信息_列表数据 {
-	序号: string;
-	收费对象: string;
-	业主名称: string;
-	手机号: string;
-	开始时间: string;
-	结束时间: string;
-	合计: string;
-	更新时间: string;
-}
-const tableDataItem: 欠费信息_列表数据 = {
-	序号: "序号",
-	收费对象: "收费对象",
-	业主名称: "业主名称",
-	手机号: "手机号",
-	开始时间: "开始时间",
-	结束时间: "结束时间",
-	合计: "合计",
-	更新时间: "更新时间",
+/**
+ * 表格搜索栏 双向绑定的变量 原本的数据
+ * @description
+ * 为了满足搜索栏组件的校验需求 这里需要额外拓展为索引类型
+ */
+const plusSearchModelRef: FieldValues & 欠费信息_列表查询_VO = {
+	收费对象: "",
+	业主名称: "",
+	手机号: "",
+	开始时间: "",
+	结束时间: "",
+	欠费时间范围: ["", ""],
+	更新时间范围: ["", ""],
 };
+
+/** 表格搜索栏 重置功能用的默认数据 */
+const plusSearchDefaultValues = cloneDeep(plusSearchModelRef);
+
+/** 表格搜索栏变量 双向绑定的变量 响应式数据 */
+const plusSearchModel = ref(plusSearchModelRef);
+
 /** 表格数据 */
-const tableData = ref<欠费信息_列表数据[]>(
-	Array(35)
-		.fill(null)
-		.map(() => ({ ...tableDataItem })),
-);
+const tableData = ref<欠费信息_列表数据[]>([]);
+
+/** 加载表格数据 */
+async function loadTableData() {
+	try {
+		/** TODO: 替换为真实的API调用 */
+		/** 当前使用模拟数据和本地搜索过滤 */
+		let filteredData = mockTableData;
+
+		// 根据搜索条件过滤数据
+		if (plusSearchModel.value.收费对象) {
+			filteredData = filteredData.filter((item) => item.收费对象 === plusSearchModel.value.收费对象);
+		}
+		if (plusSearchModel.value.业主名称) {
+			filteredData = filteredData.filter((item) => item.业主名称.includes(plusSearchModel.value.业主名称!));
+		}
+		if (plusSearchModel.value.手机号) {
+			filteredData = filteredData.filter((item) => item.手机号.includes(plusSearchModel.value.手机号!));
+		}
+		if (plusSearchModel.value.开始时间 && plusSearchModel.value.结束时间) {
+			filteredData = filteredData.filter((item) => {
+				const itemStartTime = new Date(item.开始时间).getTime();
+				const itemEndTime = new Date(item.结束时间).getTime();
+				const startTime = new Date(String(plusSearchModel.value.开始时间)).getTime();
+				const endTime = new Date(String(plusSearchModel.value.结束时间)).getTime();
+				return itemStartTime >= startTime && itemEndTime <= endTime;
+			});
+		}
+		if (plusSearchModel.value.更新时间范围) {
+			const [startTime, endTime] = plusSearchModel.value.更新时间范围;
+			if (startTime && endTime) {
+				filteredData = filteredData.filter((item) => {
+					const updateTime = new Date(item.更新时间).getTime();
+					const start = new Date(String(startTime)).getTime();
+					const end = new Date(String(endTime)).getTime();
+					return updateTime >= start && updateTime <= end;
+				});
+			}
+		}
+
+		/** 更新总数 */
+		pagination.value.total = filteredData.length;
+
+		/** 分页处理 */
+		const startIndex = (pagination.value.currentPage - 1) * pagination.value.pageSize;
+		const endIndex = startIndex + pagination.value.pageSize;
+		tableData.value = filteredData.slice(startIndex, endIndex);
+
+		/** 更新表格配置 */
+		pureTableProps.value.data = tableData.value.map((item) => ({
+			...item,
+			欠费时间段: `${item.开始时间} 至 ${item.结束时间}`,
+		}));
+	} catch (error) {
+		console.error("加载数据失败:", error);
+		/** TODO: 显示错误提示 */
+	}
+}
 
 /** 表格列配置 */
 const columns = ref<TableColumnList>([
+	defaultPureTableIndexColumn,
 	{
-		prop: "序号",
-		label: "序号",
-		width: 120,
-	},
-	{
-		prop: "收费对象",
 		label: "收费对象",
-		width: 120,
+		prop: "收费对象",
+		width: 100,
 	},
 	{
-		prop: "业主名称",
 		label: "业主名称",
+		prop: "业主名称",
 		width: 120,
 	},
 	{
-		prop: "手机号",
 		label: "手机号",
-		width: 120,
+		prop: "手机号",
+		width: 140,
 	},
 	{
-		prop: "开始时间",
-		label: "开始时间",
-		width: 120,
+		label: "欠费时间段",
+		prop: "欠费时间段",
+		width: 200,
 	},
 	{
-		prop: "结束时间",
-		label: "结束时间",
-		width: 120,
-	},
-	{
+		label: "合计金额",
 		prop: "合计",
-		label: "合计",
 		width: 120,
 	},
 	{
-		prop: "更新时间",
 		label: "更新时间",
-		width: 120,
+		prop: "更新时间",
+		width: 180,
 	},
 	{
-		label: transformI18n($t("common.table.operation")),
-		minWidth: 240,
+		/** @see https://vscode.dev/github/pure-admin/pure-admin-table/blob/main/src/columns.tsx#L36 */
+		headerRenderer: () => transformI18n($t("common.table.operation")),
+		width: 280,
 		fixed: "right",
 		slot: "operation",
 	},
@@ -93,20 +145,89 @@ const pagination = ref<PaginationProps>({
 	...defaultPagination,
 	pageSize: 10,
 	currentPage: 1,
-	total: 1000,
+	total: 0,
 });
+
+/** 重置搜索条件并重新加载数据 */
+async function handleReSearch() {
+	plusSearchModel.value = cloneDeep(plusSearchDefaultValues);
+	pagination.value.currentPage = 1;
+	await loadTableData();
+}
+
+/** 执行搜索 */
+async function handleSearch() {
+	pagination.value.currentPage = 1;
+	await loadTableData();
+}
 
 /** 处理页数变化 */
 async function handlePageSizeChange(pageSize: number) {
 	pagination.value.pageSize = pageSize;
-	// 做异步接口请求
+	await loadTableData();
 }
+
 /** 处理页码变化 即后端的 pageIndex */
 async function handleCurrentPageChange(currentPage: number) {
 	pagination.value.currentPage = currentPage;
-	// 做异步接口请求
+	await loadTableData();
 }
-/** 表格组件 配置 */
+
+/**
+ * 表格搜索栏组件 表单配置
+ * @see https://github.com/plus-pro-components/plus-pro-components/issues/184
+ */
+const plusSearchColumns = computed<PlusColumn[]>(() => [
+	// 收费对象
+	{
+		label: "收费对象",
+		prop: "收费对象",
+		valueType: "select",
+		options: 收费对象Options,
+	},
+	// 业主名称
+	{
+		label: "业主名称",
+		prop: "业主名称",
+		valueType: "input",
+	},
+	// 手机号
+	{
+		label: "手机号",
+		prop: "手机号",
+		valueType: "input",
+	},
+	// 欠费时间范围
+	{
+		label: "欠费时间范围",
+		prop: "欠费时间范围",
+		valueType: "date-picker",
+		fieldProps: {
+			type: "daterange",
+			valueFormat: "YYYY-MM-DD",
+			format: "YYYY-MM-DD",
+			onChange(value: string[] | null) {
+				plusSearchModel.value.开始时间 = value?.[0] ?? "";
+				plusSearchModel.value.结束时间 = value?.[1] ?? "";
+			},
+			onClear() {
+				plusSearchModel.value.开始时间 = "";
+				plusSearchModel.value.结束时间 = "";
+			},
+		},
+	},
+]);
+
+/** 表格搜索栏组件 配置 */
+const plusSearchProps = ref<PlusSearchProps>({
+	defaultValues: plusSearchDefaultValues,
+	columns: [],
+	labelWidth: 140,
+	labelPosition: "right",
+	showNumber: 3,
+});
+
+/** 表格配置 */
 const pureTableProps = ref<PureTableProps>({
 	...defaultPureTableProps,
 	data: tableData.value,
@@ -119,101 +240,60 @@ const pureTableBarProps = ref<PureTableBarProps>({
 	title: "欠费信息",
 	columns: columns.value,
 });
-interface 欠费信息_列表查询_VO {
-	序号?: string;
-	收费对象?: string;
-	业主名称?: string;
-	手机号?: string;
-	开始时间?: string;
-	结束时间?: string;
-	合计?: string;
-	更新时间?: string;
-}
-/**
- * 表格搜索栏 双向绑定的变量 原本的数据
- * @description
- * 为了满足搜索栏组件的校验需求 这里需要额外拓展为索引类型
- */
-const plusSearchDefaultValues = ref<Record<string, any>>({
-	序号: "",
-	收费对象: "",
-	业主名称: "",
-	手机号: "",
-	开始时间: "",
-	结束时间: "",
-	合计: "",
-	更新时间: "",
-});
 
-// TODO: 重写你的代码 不要无脑的复制粘贴
-/** 表格搜索栏 重置功能用的默认数据 */
-// const plusSearchDefaultValues = cloneDeep(plusSearchModelRef);
-
-// TODO: 重写你的代码 不要无脑的复制粘贴
-/** 表格搜索栏变量 双向绑定的变量 响应式数据 */
-// const plusSearchModel = ref(plusSearchModelRef);
-
-/**
- * 表格搜索栏组件 表单配置
- * @see https://github.com/plus-pro-components/plus-pro-components/issues/184
- */
-// TODO: 重写你的代码 不要无脑的复制粘贴 哪里来的 PlusSearchFormProps 类型？
-const plusSearchFormProps = ref<PlusSearchFormProps>({
-	//楼栋
-	columns: [
-		{
-			prop: "请选择楼栋",
-			label: "请选择楼栋",
-			valueType: "input",
-		},
-		// 单元
-		{
-			prop: "请选择单元",
-			label: "请选择单元",
-			valueType: "select",
-			options: [
-				{ label: "单元1", value: "单元1" },
-				{ label: "单元2", value: "单元2" },
-			],
-		},
-		//房屋编号
-		{
-			prop: "请输入房屋编号",
-			label: "请输入房屋编号",
-			valueType: "input",
-		},
-	],
-});
-/** 表格搜索栏组件 配置  */
-const plusSearchProps = ref<PlusSearchProps>({
-	// TODO: 重写你的代码 不要无脑的复制粘贴
-	// defaultValues: plusSearchDefaultValues,
-	columns: [],
-	labelWidth: 140,
-	labelPosition: "right",
-	showNumber: 3,
-});
-
-async function handleReSearch() {
-	console.log("重新搜索");
+/** 操作按钮点击处理 */
+function handleOperationClick(operation: string, row: 欠费信息_列表数据) {
+	console.log(`${operation} 操作`, row);
 }
 
-async function handleSearch() {
-	console.log("搜索");
-}
+onMounted(async () => {
+	await loadTableData();
+});
 </script>
 
 <template>
 	<section class="index-root">
-		<h2>欠费信息</h2>
-		<!-- @vue-ignore 忽略treeProps所需要的checkStrictly类型 -->
-		<PureTable :="pureTableProps">
-			<template #operation="{ row }">
-				<ElButton type="default">{{ transformI18n($t("欠费缴费")) }}</ElButton>
-				<ElButton type="info">{{ transformI18n($t("common.buttons.info")) }}</ElButton>
-				<ElButton type="default">{{ transformI18n($t("查看费用")) }}</ElButton>
+		<PlusSearch
+			v-model="plusSearchModel"
+			:="plusSearchProps"
+			:columns="plusSearchColumns"
+			@search="handleSearch"
+			@reset="handleReSearch"
+		/>
+
+		<PureTableBar :="pureTableBarProps" @refresh="handleReSearch">
+			<template #buttons>
+				<ElButton type="info">
+					{{ transformI18n($t("欠费缴费")) }}
+				</ElButton>
+				<ElButton type="primary">
+					{{ transformI18n($t("导出欠费清单")) }}
+				</ElButton>
 			</template>
-		</PureTable>
+
+			<template #default="{ size, dynamicColumns }">
+				<!-- @vue-ignore 忽略treeProps所需要的checkStrictly类型 -->
+				<PureTable
+					:="pureTableProps"
+					:columns="dynamicColumns"
+					:size="size"
+					@page-size-change="handlePageSizeChange"
+					@page-current-change="handleCurrentPageChange"
+				>
+					<template #operation="{ row }">
+						<ElButton type="primary" @click="handleOperationClick('欠费缴费', row)">
+							{{ transformI18n($t("欠费缴费")) }}
+						</ElButton>
+						<ElButton type="warning" @click="handleOperationClick('查看详情', row)">
+							{{ transformI18n($t("common.buttons.info")) }}
+						</ElButton>
+						<ElButton type="info" @click="handleOperationClick('查看费用', row)">
+							{{ transformI18n($t("查看费用")) }}
+						</ElButton>
+					</template>
+				</PureTable>
+			</template>
+		</PureTableBar>
 	</section>
 </template>
 
