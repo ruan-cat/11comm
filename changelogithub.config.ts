a20a619821f8d253fdbdd8f86b9f0f09cb6f702c
ruan-cat <1219043956@qq.com>
2025-11-11 00:58:01 +0800

    🎉 init(package.json,config,root): 初始化配置 changelogithub 。

---
+++ changelogithub.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "changelogithub";
+
+/**
+ * @see https://github.com/antfu/changelogithub
+ */
+export default defineConfig({
+	output: "CHANGELOG.md",
+	types: {},
+});
