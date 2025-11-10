c235e6ef97d46fe4e5bc22fb500eb1b9f0642b3c
ruan-cat <1219043956@qq.com>
2025-11-11 02:16:23 +0800

    🔧 config(package.json,config,root): 尝试给 changelogithub 配置文件增加范围。查看发版效果。

--- changelogithub.config.ts
+++ changelogithub.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "changelogithub";
 
+import { commitTypes } from "@ruan-cat/commitlint-config/src/commit-types.ts";
+
 /**
  * @see https://github.com/antfu/changelogithub
  */
 export default defineConfig({
 	output: "CHANGELOG.md",
-	types: {},
+
+	/** @see https://github.com/viapip/ozon-tracker/blob/master/changelogen.config.json */
+	types: Object.fromEntries(
+		commitTypes.map((commitType) => [
+			commitType.type,
+			{
+				title: commitType.type.charAt(0).toUpperCase() + commitType,
+			},
+		]),
+	),
 });
