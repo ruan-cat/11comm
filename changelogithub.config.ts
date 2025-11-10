d051f3da75185f63efb6e212940d7b347886d478
ruan-cat <1219043956@qq.com>
2025-11-11 02:29:36 +0800

    🐳 chore(config): 尝试实现写入本地 CHANGELOG.md 文件。

--- changelogithub.config.ts
+++ changelogithub.config.ts
@@ -6,15 +6,19 @@ import { commitTypes } from "@ruan-cat/commitlint-config/src/commit-types.ts";
  * @see https://github.com/antfu/changelogithub
  */
 export default defineConfig({
-	output: "CHANGELOG.md",
+	// 尝试让工作流自己生成文件
+	// output: "CHANGELOG.md",
 
 	/** @see https://github.com/viapip/ozon-tracker/blob/master/changelogen.config.json */
 	types: Object.fromEntries(
-		commitTypes.map((commitType) => [
-			commitType.type,
-			{
-				title: commitType.type.charAt(0).toUpperCase() + commitType,
-			},
-		]),
+		commitTypes.map((commitType) => {
+			const { type, description, emoji } = commitType;
+			return [
+				type,
+				{
+					title: `${emoji} ${description}`,
+				},
+			];
+		}),
 	),
 });
