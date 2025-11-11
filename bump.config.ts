d3d2168cda3bbbfbfa7a55ab37cae83ad411d9dd
ruan-cat <1219043956@qq.com>
2025-11-12 00:15:22 +0800

    🔧 config(config,root): 尝试不生成 tag

--- bump.config.ts
+++ bump.config.ts
@@ -8,7 +8,9 @@ export default defineConfig({
 	// 不生成提交信息
 	commit: false,
 	// commit: "📢 publish: release package v%s",
-	tag: "v%s",
+	// tag: "v%s",
+	// 尝试不生成 tag
+	tag: false,
 	// 不推送到远程仓库
 	push: false,
 });
