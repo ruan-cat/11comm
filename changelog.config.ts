4bda26a55b54660e16203bcd292947fc6087387f
ruan-cat <1219043956@qq.com>
2025-11-11 20:31:25 +0800

    🔧 config(config,root): 新建 changelogen 的配置文件，尝试生成美观的更新日志。

---
+++ changelog.config.ts
@@ -0,0 +1,28 @@
+import { type ChangelogConfig } from "changelogen";
+
+import { commitTypes } from "@ruan-cat/commitlint-config/src/commit-types.ts";
+
+/**
+ * @see https://github.com/unjs/changelogen
+ */
+export default {
+	// 尝试让工作流自己生成文件
+	// output: "CHANGELOG.md",
+
+	/** @see https://github.com/viapip/ozon-tracker/blob/master/changelogen.config.json */
+	types: Object.fromEntries(
+		commitTypes.map((commitType) => {
+			const { type, description, emoji } = commitType;
+			return [
+				type,
+				{
+					title: `${emoji} ${description}`,
+				},
+			];
+		}),
+	),
+
+	templates: {
+		commitMessage: "📢 publish: release package v{{newVersion}}",
+	},
+} satisfies Partial<ChangelogConfig>;
