569708b7e21969d1b6e2c0eda7d475874c59ad5d
ruan-cat <1219043956@qq.com>
2025-11-26 03:10:41 +0800

    🔧 config(package.json,config): 预备 commit-and-tag-version 的生成命令。

--- bump.config.ts
+++ bump.config.ts
@@ -13,8 +13,9 @@ export default defineConfig({
 	// tag: false,
 	// 不推送到远程仓库
 	// push: false,
-	// 在执行完 bumpp 后执行 changelog 命令
+	// 在执行完 bumpp 后执行本地的更新日志生成命令
 	execute: "pnpm run changelog:conventional-changelog",
+	// execute: "pnpm run changelog:commit-and-tag-version",
 	// 将暂存区的全部文件都提交
 	all: true,
 });
