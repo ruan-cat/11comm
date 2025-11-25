995356597033423718bb0bc9b71faea60a25704b
ruan-cat <1219043956@qq.com>
2025-11-26 03:54:06 +0800

    🔧 config!: 关闭掉 commit-and-tag-version 的一部分默认行为。

--- .versionrc.js
+++ .versionrc.js
@@ -6,13 +6,14 @@ module.exports = {
 	skip: {
 		// 取得当前版本（比如package.json里面的version字段，这里我们定义了packageFiles，所以会从packageFiles.filename取），
 		// 升版本：1.0.0 => 1.1.0 或者 1.0.0 => 2.0.0等（如何升级可以由参数控制）
+		// 增加bump能力交由其他工具实现
 		bump: false,
 		// 自动产出changelog文档
 		changelog: true,
-		// 提交变动
-		commit: true,
-		// 在git中增加tag标识
-		tag: true,
+		// 提交变动 提交能力交由其他工具实现
+		commit: false,
+		// 在git中增加tag标识 增加tag能力交由其他工具实现
+		tag: false,
 	},
 	types: commitTypes.map((commitType) => {
 		const { type, description, emoji } = commitType;
