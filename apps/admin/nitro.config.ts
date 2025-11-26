84cc05d374c795961719e78159aad2892da0a5a3
ruan-cat <1219043956@qq.com>
2025-11-27 04:18:42 +0800

    🔧 config(config,admin): 排除掉 nitro 插件。
    目前没有办法实现 nitro 打包 整个项目时无法运行的 ， 出现 SSR 渲染问题。无法正确识别项目中的路径别名。

--- apps/admin/nitro.config.ts
+++ apps/admin/nitro.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "nitro";
+import { pathResolve } from "./build/utils";
 
+// FIXME: 目前没有办法实现 nitro 打包 整个项目时无法运行的 ， 出现 SSR 渲染问题。无法正确识别项目中的路径别名。
 export default defineConfig({
-	serverDir: "./",
+	// serverDir: "apps/admin",
+	serverDir: false,
+	alias: {
+		"@": pathResolve("./src"),
+	},
 });
