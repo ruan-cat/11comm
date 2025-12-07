af6a4e7d1053317a117ff79cf77b7f0232bf8cdc
ruan-cat <1219043956@qq.com>
2025-12-07 20:42:50 +0800

    🦄 refactor(admin): 完成 property-manage/report-manage/repair-report-form 列表页的改造，更新状态为完成，优化表单逻辑，新增表格数据和搜索功能

--- apps/admin/src/pages/property-manage/report-manage/repair-report-form/index.vue
+++ apps/admin/src/pages/property-manage/report-manage/repair-report-form/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { ref } from "vue";
 definePage({
 	meta: {
 		title: "报修报表",
@@ -8,75 +7,324 @@ definePage({
 		rank: getRouteRank("propertyManage.reportManage.repairReportForm"),
 	},
 });
-// 定义要显示的组件
-// 导入先丑点吧，后面在改动态导入的
-import RepairItemTable from "./components/repairitemtable.vue"; // 报修分项表
-import RepairDetail from "./components/repairdetail.vue"; // 报修明细
-import RepairStatistics from "./components/repairstatistics.vue"; // 报修统计
-import RepairUnfinished from "./components/repairunfinished.vue"; // 报修未完成
-import RepairCharge from "./components/repaircharge.vue"; // 报修收费
-import RepairDissatisfied from "./components/repairdissatisfied.vue"; // 报修不满意
-import ComplaintConsult from "./components/complaintconsult.vue"; // 投诉咨询
 
-// 动态组件
-const currentComponent = ref(RepairItemTable);
+import dayjs from "dayjs";
+import { transformI18n } from "@/plugins/i18n";
+import {
+	tableData as mockTableData,
+	type 报修报表_搜索_VO,
+	type 报修报表_表格数据,
+	报修类型Options,
+	报修状态Options,
+	紧急程度Options,
+	小区Options,
+	收费状态Options,
+} from "./test-data";
 
-// 处理菜单点击
-const handleMenuClick = (index: string) => {
-	const componentMap = {
-		"1": RepairItemTable,
-		"2": RepairDetail,
-		"3": RepairStatistics,
-		"4": RepairUnfinished,
-		"5": RepairCharge,
-		"6": RepairDissatisfied,
-		"7": ComplaintConsult,
-	};
-	currentComponent.value = componentMap[index];
+/** 分页配置 */
+const pagination = ref<PaginationProps>({
+	...defaultPagination,
+	pageSize: 10,
+	currentPage: 1,
+	total: mockTableData.length,
+});
+
+/** 表格数据 */
+const tableData = ref<报修报表_表格数据[]>([]);
+
+/** 表格列配置 */
+const columns = ref<TableColumnList>([
+	defaultPureTableIndexColumn,
+	{
+		label: "小区",
+		prop: "小区",
+		minWidth: 140,
+	},
+	{
+		label: "报修单号",
+		prop: "报修单号",
+		minWidth: 160,
+	},
+	{
+		label: "报修类型",
+		prop: "报修类型",
+		minWidth: 140,
+	},
+	{
+		label: "紧急程度",
+		prop: "紧急程度",
+		minWidth: 140,
+	},
+	{
+		label: "报修人",
+		prop: "报修人",
+		minWidth: 140,
+	},
+	{
+		label: "报修电话",
+		prop: "报修电话",
+		minWidth: 160,
+	},
+	{
+		label: "报修地址",
+		prop: "报修地址",
+		minWidth: 180,
+	},
+	{
+		label: "报修时间",
+		prop: "报修时间",
+		minWidth: 180,
+	},
+	{
+		label: "受理人",
+		prop: "受理人",
+		minWidth: 140,
+	},
+	{
+		label: "处理人",
+		prop: "处理人",
+		minWidth: 140,
+	},
+	{
+		label: "费用状态",
+		prop: "费用状态",
+		minWidth: 140,
+	},
+	{
+		label: "报修状态",
+		prop: "报修状态",
+		minWidth: 140,
+	},
+	{
+		headerRenderer: () => transformI18n($t("common.table.operation")),
+		width: 230,
+		fixed: "right",
+		slot: "operation",
+	},
+]);
+
+/** 表格配置 */
+const pureTableProps = ref<PureTableProps>({
+	...defaultPureTableProps,
+	data: tableData.value,
+	columns: [],
+	pagination: pagination.value,
+});
+
+/** 表格操作栏组件 配置  */
+const pureTableBarProps = ref<PureTableBarProps>({
+	title: "报修报表",
+	columns: columns.value,
+});
+
+/**
+ * 表格搜索栏 双向绑定的变量 原本的数据
+ * @description
+ * 为了满足搜索栏组件的校验需求 这里需要额外拓展为索引类型
+ */
+const plusSearchModelRef: FieldValues & 报修报表_搜索_VO = {
+	报修类型: "",
+	报修状态: "",
+	紧急程度: "",
+	报修人: "",
+	报修电话: "",
+	小区: "",
+	报修时间开始: "",
+	报修时间结束: "",
+	费用状态: "",
 };
+
+/** 表格搜索栏 重置功能用的默认数据 */
+const plusSearchDefaultValues = cloneDeep(plusSearchModelRef);
+
+/** 表格搜索栏变量 双向绑定的变量 响应式数据 */
+const plusSearchModel = ref(plusSearchModelRef);
+
+/**
+ * 表格搜索栏组件 表单配置
+ * @see https://github.com/plus-pro-components/plus-pro-components/issues/184
+ */
+const plusSearchColumns = computed<PlusColumn[]>(() => [
+	{
+		label: "报修类型",
+		prop: "报修类型",
+		valueType: "select",
+		options: 报修类型Options,
+	},
+	{
+		label: "报修状态",
+		prop: "报修状态",
+		valueType: "select",
+		options: 报修状态Options,
+	},
+	{
+		label: "紧急程度",
+		prop: "紧急程度",
+		valueType: "select",
+		options: 紧急程度Options,
+	},
+	{
+		label: "报修人",
+		prop: "报修人",
+		valueType: "input",
+	},
+	{
+		label: "报修电话",
+		prop: "报修电话",
+		valueType: "input",
+	},
+	{
+		label: "小区",
+		prop: "小区",
+		valueType: "select",
+		options: 小区Options,
+	},
+	{
+		label: "费用状态",
+		prop: "费用状态",
+		valueType: "select",
+		options: 收费状态Options,
+	},
+	{
+		label: "报修时间开始",
+		prop: "报修时间开始",
+		valueType: "date-picker",
+	},
+	{
+		label: "报修时间结束",
+		prop: "报修时间结束",
+		valueType: "date-picker",
+	},
+]);
+
+/** 表格搜索栏组件 配置  */
+const plusSearchProps = ref<PlusSearchProps>({
+	defaultValues: plusSearchDefaultValues,
+	columns: [],
+	labelWidth: 140,
+	labelPosition: "right",
+	showNumber: 3,
+});
+
+/** 加载表格数据 */
+async function loadTableData() {
+	let filteredData = mockTableData;
+
+	if (plusSearchModel.value.报修类型) {
+		filteredData = filteredData.filter((item) => item.报修类型 === plusSearchModel.value.报修类型);
+	}
+
+	if (plusSearchModel.value.报修状态) {
+		filteredData = filteredData.filter((item) => item.报修状态 === plusSearchModel.value.报修状态);
+	}
+
+	if (plusSearchModel.value.紧急程度) {
+		filteredData = filteredData.filter((item) => item.紧急程度 === plusSearchModel.value.紧急程度);
+	}
+
+	if (plusSearchModel.value.报修人) {
+		filteredData = filteredData.filter((item) => item.报修人.includes(plusSearchModel.value.报修人!));
+	}
+
+	if (plusSearchModel.value.报修电话) {
+		filteredData = filteredData.filter((item) => item.报修电话.includes(plusSearchModel.value.报修电话!));
+	}
+
+	if (plusSearchModel.value.小区) {
+		filteredData = filteredData.filter((item) => item.小区 === plusSearchModel.value.小区);
+	}
+
+	if (plusSearchModel.value.费用状态) {
+		filteredData = filteredData.filter((item) => item.费用状态 === plusSearchModel.value.费用状态);
+	}
+
+	if (plusSearchModel.value.报修时间开始 && plusSearchModel.value.报修时间结束) {
+		const start = dayjs(plusSearchModel.value.报修时间开始);
+		const end = dayjs(plusSearchModel.value.报修时间结束);
+		filteredData = filteredData.filter((item) => {
+			const current = dayjs(item.报修时间);
+			return current.isAfter(start) && current.isBefore(end);
+		});
+	}
+
+	pagination.value.total = filteredData.length;
+
+	const startIndex = (pagination.value.currentPage - 1) * pagination.value.pageSize;
+	const endIndex = startIndex + pagination.value.pageSize;
+	tableData.value = filteredData.slice(startIndex, endIndex);
+
+	pureTableProps.value.data = tableData.value;
+	pureTableProps.value.pagination = pagination.value;
+}
+
+/** 重置搜索条件并重新加载数据 */
+async function handleReSearch() {
+	plusSearchModel.value = cloneDeep(plusSearchDefaultValues);
+	pagination.value.currentPage = 1;
+	await loadTableData();
+}
+
+/** 执行搜索 */
+async function handleSearch() {
+	pagination.value.currentPage = 1;
+	await loadTableData();
+}
+
+/** 处理页数变化 */
+async function handlePageSizeChange(pageSize: number) {
+	pagination.value.pageSize = pageSize;
+	await loadTableData();
+}
+
+/** 处理页码变化 即后端的 pageIndex */
+async function handleCurrentPageChange(currentPage: number) {
+	pagination.value.currentPage = currentPage;
+	await loadTableData();
+}
+
+onMounted(async () => {
+	await loadTableData();
+});
 </script>
 
 <template>
 	<section class="index-root">
-		<h2>报修报表</h2>
+		<PlusSearch
+			v-model="plusSearchModel"
+			:="plusSearchProps"
+			:columns="plusSearchColumns"
+			@search="handleSearch"
+			@reset="handleReSearch"
+		/>
+
+		<PureTableBar :="pureTableBarProps" @refresh="handleReSearch">
+			<template #buttons>
+				<ElButton type="info" @click="handleReSearch">
+					{{ transformI18n($t("common.buttons.pureReload")) }}
+				</ElButton>
+			</template>
+
+			<template #default="{ size, dynamicColumns }">
+				<!-- @vue-ignore 忽略treeProps所需要的checkStrictly类型 -->
+				<PureTable
+					:="pureTableProps"
+					:columns="dynamicColumns"
+					:size="size"
+					@page-size-change="handlePageSizeChange"
+					@page-current-change="handleCurrentPageChange"
+				>
+					<template #operation="{ row }">
+						<ElButton type="info">
+							{{ transformI18n($t("common.buttons.info")) }}
+						</ElButton>
+					</template>
+				</PureTable>
+			</template>
+		</PureTableBar>
 	</section>
-	<el-row class="tac">
-		<el-col :span="4">
-			<el-menu default-active="1" class="el-menu-vertical-demo" @select="handleMenuClick">
-				<el-menu-item index="1">
-					<span>报修分项表</span>
-				</el-menu-item>
-				<el-menu-item index="2">
-					<span>报修明细</span>
-				</el-menu-item>
-				<el-menu-item index="3">
-					<span>报修统计</span>
-				</el-menu-item>
-				<el-menu-item index="4">
-					<span>报修未完成</span>
-				</el-menu-item>
-				<el-menu-item index="5">
-					<span>报修收费</span>
-				</el-menu-item>
-				<el-menu-item index="6">
-					<span>报修不满意</span>
-				</el-menu-item>
-				<el-menu-item index="7">
-					<span>投诉咨询</span>
-				</el-menu-item>
-			</el-menu>
-		</el-col>
-		<el-col :span="20">
-			<!-- 动态组件 -->
-			<component :is="currentComponent" />
-		</el-col>
-	</el-row>
 </template>
 
 <style lang="scss" scoped>
 .index-root {
-}
-.tac {
-	height: calc(100vh - 100px);
 }
 </style>
