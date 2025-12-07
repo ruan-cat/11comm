3ee33eaadbe5d86b398107979f89da9272398cd4
ruan-cat <1219043956@qq.com>
2025-12-07 19:55:08 +0800

    🦄 refactor(admin): 完成 property-manage/report-manage/data-statistics 列表页的改造，更新状态为完成，优化表单逻辑，新增测试数据和搜索功能

--- apps/admin/src/pages/property-manage/report-manage/data-statistics/index.vue
+++ apps/admin/src/pages/property-manage/report-manage/data-statistics/index.vue
@@ -8,185 +8,121 @@ definePage({
 	},
 });
 
-import { ref, computed, watch } from "vue";
+import dayjs from "dayjs";
 import { transformI18n } from "@/plugins/i18n";
-import { title } from "process";
-import feeclass from "./components/class/feeclass.vue";
-import orderclass from "./components/class/orderclass.vue";
-import entryoutclass from "./components/class/entryoutclass.vue";
-import otherclass from "./components/class/otherclass.vue";
-import type { TabsPaneContext } from "element-plus";
-// import * as TableComponents from "./components/table";
-
-// export default {
-//   components: {
-//     ...TableComponents,
-//   },
-// };
-import shishoutongji from "./components/table/shishoutongji.vue";
-import shishoumingxi from "./components/table/shishoumingxi.vue";
-import shoukuanfangshi from "./components/table/shoukuanfangshi.vue";
-import qianfeitongji from "./components/table/qianfeitongji.vue";
-import qianfeimingxi from "./components/table/qianfeimingxi.vue";
-import shoujiaoqingkuang from "./components/table/shoujiaoqingkuang.vue";
-import yueshishoumingxi from "./components/table/yueshishoumingxi.vue";
-import yueqianfeimingxi from "./components/table/yueqianfeimingxi.vue";
-
-//elmentplus分页
-const activeName = ref("first");
-
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-	console.log(tab, event);
-};
-
-interface 报表管理_数据统计 {
-	房屋: string;
-	业主: string;
-	欠费: string;
-	实收: string;
-	物业费: string;
-	押金: string;
-	停车费: string;
-	煤气费: string;
-	取暖费: string;
-	维修费: string;
-	服务费: string;
-	其他: string;
-	水费: string;
-	电费: string;
-	租金: string;
-	公摊费: string;
-}
-
-const tableDataItem: 报表管理_数据统计 = {
-	房屋: "20250530-112725-101",
-	业主: "20250530业主(13555555555)",
-	欠费: "1000",
-	实收: "0",
-	物业费: "111.00",
-	押金: "0",
-	停车费: "0",
-	煤气费: "0",
-	取暖费: "0",
-	维修费: "0",
-	服务费: "0",
-	其他: "0",
-	水费: "0",
-	电费: "0",
-	租金: "0",
-	公摊费: "0",
-};
-
-/** 表格数据 */
-const tableData = ref<报表管理_数据统计[]>(
-	Array(35)
-		.fill(null)
-		.map(() => ({ ...tableDataItem })),
-);
-// 表格列配置
-const columns = ref<TableColumnList>([
-	{
-		label: "房屋",
-		prop: "房屋",
-		width: 60,
-		fixed: true,
-	},
-	{
-		label: "业主",
-		prop: "业主",
-		width: 120,
-	},
-	{
-		label: "欠费",
-		prop: "欠费",
-		width: 160,
-	},
-	{
-		label: "实收",
-		prop: "实收",
-		width: 120,
-	},
-	{
-		label: "物业费",
-		prop: "物业费",
-		width: 180,
-	},
-	{
-		label: "押金",
-		prop: "押金",
-		width: 180,
-	},
-	{
-		label: "停车费",
-		prop: "停车费",
-		width: 100,
-	},
-	{
-		label: "煤气费",
-		prop: "结束时间",
-		width: 80,
-	},
-	{
-		label: "取暖费",
-		prop: "取暖费",
-		width: 80,
-	},
-	{
-		label: "服务费",
-		prop: "服务费",
-		width: 80,
-	},
-	{
-		label: "其他",
-		prop: "其他费",
-		width: 80,
-	},
-	{
-		label: "水费",
-		prop: "水费",
-		width: 80,
-	},
-	{
-		label: "电费",
-		prop: "电费",
-		width: 80,
-	},
-	{
-		label: "租金",
-		prop: "租金",
-		width: 80,
-	},
-	{
-		label: "公摊费",
-		prop: "公摊费",
-		width: 80,
-	},
-	// {
-	// 	label: transformI18n($t("table.operation")),
-	// 	minWidth: 240,
-	// 	fixed: "right",
-	// 	slot: "operation",
-	// },
-]);
+import {
+	tableData as mockTableData,
+	type 报表管理_数据统计_搜索_VO,
+	type 报表管理_数据统计_表格数据,
+	小区Options,
+} from "./test-data";
 
 /** 分页配置 */
 const pagination = ref<PaginationProps>({
 	...defaultPagination,
 	pageSize: 10,
 	currentPage: 1,
-	total: 1000,
+	total: mockTableData.length,
 });
 
-/** 处理页数变化 */
-async function handlePageSizeChange(pageSize: number) {
-	pagination.value.pageSize = pageSize;
-	// 做异步接口请求
-}
-/** 处理页码变化 即后端的 pageIndex */
-async function handleCurrentPageChange(currentPage: number) {
-	pagination.value.currentPage = currentPage;
-	// 做异步接口请求
-}
+/** 表格数据 */
+const tableData = ref<报表管理_数据统计_表格数据[]>([]);
+
+/** 表格列配置 */
+const columns = ref<TableColumnList>([
+	defaultPureTableIndexColumn,
+	{
+		label: "小区",
+		prop: "小区",
+		minWidth: 160,
+	},
+	{
+		label: "房屋",
+		prop: "房屋",
+		minWidth: 160,
+	},
+	{
+		label: "业主",
+		prop: "业主",
+		minWidth: 180,
+	},
+	{
+		label: "欠费",
+		prop: "欠费",
+		minWidth: 120,
+	},
+	{
+		label: "实收",
+		prop: "实收",
+		minWidth: 120,
+	},
+	{
+		label: "物业费",
+		prop: "物业费",
+		minWidth: 120,
+	},
+	{
+		label: "押金",
+		prop: "押金",
+		minWidth: 120,
+	},
+	{
+		label: "停车费",
+		prop: "停车费",
+		minWidth: 120,
+	},
+	{
+		label: "煤气费",
+		prop: "煤气费",
+		minWidth: 120,
+	},
+	{
+		label: "取暖费",
+		prop: "取暖费",
+		minWidth: 120,
+	},
+	{
+		label: "维修费",
+		prop: "维修费",
+		minWidth: 120,
+	},
+	{
+		label: "服务费",
+		prop: "服务费",
+		minWidth: 120,
+	},
+	{
+		label: "其他",
+		prop: "其他",
+		minWidth: 120,
+	},
+	{
+		label: "水费",
+		prop: "水费",
+		minWidth: 120,
+	},
+	{
+		label: "电费",
+		prop: "电费",
+		minWidth: 120,
+	},
+	{
+		label: "租金",
+		prop: "租金",
+		minWidth: 120,
+	},
+	{
+		label: "公摊费",
+		prop: "公摊费",
+		minWidth: 120,
+	},
+	{
+		headerRenderer: () => transformI18n($t("common.table.operation")),
+		width: 230,
+		fixed: "right",
+		slot: "operation",
+	},
+]);
 
 /** 表格配置 */
 const pureTableProps = ref<PureTableProps>({
@@ -196,24 +132,19 @@ const pureTableProps = ref<PureTableProps>({
 	pagination: pagination.value,
 });
 
-// 表格操作栏组件配置
+/** 表格操作栏组件配置 */
 const pureTableBarProps = ref<PureTableBarProps>({
 	title: "数据统计",
 	columns: columns.value,
 });
 
-interface 报表管理_费用明细表_VO {
-	开始事件?: string;
-	结束时间?: string;
-	小区?: string;
-}
 /**
  * 表格搜索栏 双向绑定的变量 原本的数据
  * @description
  * 为了满足搜索栏组件的校验需求 这里需要额外拓展为索引类型
  */
-const plusSearchModelRef: FieldValues & 报表管理_费用明细表_VO = {
-	开始事件: "",
+const plusSearchModelRef: FieldValues & 报表管理_数据统计_搜索_VO = {
+	开始时间: "",
 	结束时间: "",
 	小区: "",
 };
@@ -228,49 +159,22 @@ const plusSearchModel = ref(plusSearchModelRef);
  * 表格搜索栏组件 表单配置
  * @see https://github.com/plus-pro-components/plus-pro-components/issues/184
  */
-
 const plusSearchColumns = computed<PlusColumn[]>(() => [
-	// 开始时间
 	{
-		label: transformI18n($t("propertyManage_reportManage.report.startTime")),
+		label: "开始时间",
 		prop: "开始时间",
 		valueType: "date-picker",
-		fieldProps: {
-			type: "datetime",
-			valueFormat: "YYYY-MM-DD HH:mm:ss",
-			format: "YYYY-MM-DD HH:mm:ss",
-		},
 	},
-	// 结束时间
 	{
-		label: transformI18n($t("propertyManage_reportManage.report.endTime")),
+		label: "结束时间",
 		prop: "结束时间",
 		valueType: "date-picker",
-		fieldProps: {
-			type: "datetime",
-			valueFormat: "YYYY-MM-DD HH:mm:ss",
-			format: "YYYY-MM-DD HH:mm:ss",
-		},
 	},
-	//小区
 	{
-		label: transformI18n($t("propertyManage_reportManage.report.cell")),
+		label: "小区",
 		prop: "小区",
 		valueType: "select",
-		options: [
-			{
-				label: "小区1",
-				value: "小区1",
-			},
-			{
-				label: "小区2",
-				value: "小区2",
-			},
-			{
-				label: "小区3",
-				value: "小区3",
-			},
-		],
+		options: 小区Options,
 	},
 ]);
 
@@ -283,60 +187,96 @@ const plusSearchProps = ref<PlusSearchProps>({
 	showNumber: 3,
 });
 
+/** 加载表格数据 */
+async function loadTableData() {
+	let filteredData = mockTableData;
+
+	if (plusSearchModel.value.小区) {
+		filteredData = filteredData.filter((item) => item.小区 === plusSearchModel.value.小区);
+	}
+
+	if (plusSearchModel.value.开始时间 && plusSearchModel.value.结束时间) {
+		const start = dayjs(plusSearchModel.value.开始时间);
+		const end = dayjs(plusSearchModel.value.结束时间);
+		filteredData = filteredData.filter((item) => {
+			const current = dayjs(item.统计时间);
+			return current.isAfter(start) && current.isBefore(end);
+		});
+	}
+
+	pagination.value.total = filteredData.length;
+
+	const startIndex = (pagination.value.currentPage - 1) * pagination.value.pageSize;
+	const endIndex = startIndex + pagination.value.pageSize;
+	tableData.value = filteredData.slice(startIndex, endIndex);
+
+	pureTableProps.value.data = tableData.value;
+	pureTableProps.value.pagination = pagination.value;
+}
+
+/** 重置搜索条件并重新加载数据 */
 async function handleReSearch() {
-	console.log("重新搜索");
+	plusSearchModel.value = cloneDeep(plusSearchDefaultValues);
+	pagination.value.currentPage = 1;
+	await loadTableData();
 }
+
+/** 执行搜索 */
 async function handleSearch() {
-	console.log("搜索");
+	pagination.value.currentPage = 1;
+	await loadTableData();
 }
+
+/** 处理页数变化 */
+async function handlePageSizeChange(pageSize: number) {
+	pagination.value.pageSize = pageSize;
+	await loadTableData();
+}
+
+/** 处理页码变化 即后端的 pageIndex */
+async function handleCurrentPageChange(currentPage: number) {
+	pagination.value.currentPage = currentPage;
+	await loadTableData();
+}
+
+onMounted(async () => {
+	await loadTableData();
+});
 </script>
 
 <template>
 	<section class="index-root">
-		<PlusSearch v-model="plusSearchModel" :="plusSearchProps" :columns="plusSearchColumns" @search="handleReSearch" />
-		<!-- 横向排列的容器 -->
-		<div class="module-container">
-			<feeclass />
-			<orderclass />
-			<entryoutclass />
-			<otherclass />
-		</div>
-		<!-- {{ plusSearchModel }} -->
+		<PlusSearch
+			v-model="plusSearchModel"
+			:="plusSearchProps"
+			:columns="plusSearchColumns"
+			@search="handleSearch"
+			@reset="handleReSearch"
+		/>
 
-		<el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-			<el-tab-pane label="实收统计" name="first"><shishoutongji /> </el-tab-pane>
-			<el-tab-pane label="实收明细" name="second"><shishoumingxi /> </el-tab-pane>
-			<el-tab-pane label="收款方式统计" name="third"><shoukuanfangshi /> </el-tab-pane>
-			<el-tab-pane label="欠费统计" name="fourth"><qianfeitongji /> </el-tab-pane>
-			<el-tab-pane label="欠费明细" name="fifth"><qianfeimingxi /> </el-tab-pane>
-			<el-tab-pane label="收缴情况" name="sixth"><shoujiaoqingkuang /> </el-tab-pane>
-			<el-tab-pane label="月实收明细" name="seventh"><yueshishoumingxi /> </el-tab-pane>
-			<el-tab-pane label="月欠费明细" name="eighth"><yueqianfeimingxi /> </el-tab-pane>
-		</el-tabs>
+		<PureTableBar :="pureTableBarProps" @refresh="handleReSearch">
+			<template #buttons>
+				<ElButton type="info" @click="handleReSearch">
+					{{ transformI18n($t("common.buttons.pureReload")) }}
+				</ElButton>
+			</template>
+
+			<template #default="{ size, dynamicColumns }">
+				<!-- @vue-ignore 忽略treeProps所需要的checkStrictly类型 -->
+				<PureTable
+					:="pureTableProps"
+					:columns="dynamicColumns"
+					:size="size"
+					@page-size-change="handlePageSizeChange"
+					@page-current-change="handleCurrentPageChange"
+				>
+					<template #operation="{ row }">
+						<ElButton type="info">
+							{{ transformI18n($t("common.buttons.info")) }}
+						</ElButton>
+					</template>
+				</PureTable>
+			</template>
+		</PureTableBar>
 	</section>
 </template>
-
-<style lang="scss" scoped>
-.index-root {
-}
-/* 横向排列的容器 */
-.module-container {
-	display: flex; /* 启用 Flex 布局 */
-	gap: 20px; /* 组件之间的间隔 */
-
-	justify-content: space-between; /* 左右对齐，中间均匀分布 */
-	margin-bottom: 20px; /* 下方留出空间 */
-}
-
-/* 可选：统一设置每个模块的宽度 */
-.module-container > * {
-	flex: 1 1 22%; /* 每个模块最小宽度 22%，自动扩展 */
-	min-width: 100px; /* 最小宽度限制 */
-}
-.demo-tabs > .el-tabs__content {
-	padding: 32px;
-	color: #6b778c;
-	font-size: 32px;
-	font-weight: 600;
-}
-</style>
