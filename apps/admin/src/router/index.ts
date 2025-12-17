e7c1ee466f9bd29760dfa2af6f2ab311cfa11bca
ruan-cat <1219043956@qq.com>
2025-12-17 21:10:32 +0800

    🐞 fix(router,admin)!: 彻底修复了自动路由模块混入服务端代码的错误。

--- apps/admin/src/router/index.ts
+++ apps/admin/src/router/index.ts
@@ -18,27 +18,53 @@ import {
 	handleAliveRoute,
 	formatTwoStageRoutes,
 	formatFlatteningRoutes,
+	/** @see https://pure-admin.cn/pages/routerMenu/#如何只要静态路由 */
+	addPathMatch,
 } from "./utils";
 import { type Router, type RouteRecordRaw, type RouteComponent, createRouter } from "vue-router";
 import { type DataInfo, userKey, removeToken, multipleTabsKey } from "@/utils/auth";
 
 import { routes as autoRoutes, handleHotUpdate } from "vue-router/auto-routes";
 import { setupLayouts } from "virtual:meta-layouts";
+import { disposalAutoRouter } from "@ruan-cat/utils/unplugin-vue-router";
 
 /** 自动导入全部静态路由，无需再手动引入！匹配 src/router/modules 目录（任何嵌套级别）中具有 .ts 扩展名的所有文件，除了 remaining.ts 文件
  * 如何匹配所有文件请看：https://github.com/mrmlnc/fast-glob#basic-syntax
  * 如何排除文件请看：https://cn.vitejs.dev/guide/features.html#negative-patterns
  */
-const modules: Record<string, any> = import.meta.glob(["./modules/**/*.ts", "!./modules/**/remaining.ts"], {
-	eager: true,
-});
+const modules: Record<string, any> = import.meta.glob(
+	//
+	["./modules/**/*.ts", "!./modules/**/remaining.ts"],
+	{ eager: true },
+);
+
+/** 导入框架自带的路由 其他的路由均不导入 */
+const routerModulesPureAdmin: Record<string, any> = import.meta.glob(
+	["./modules/**/error.ts", "./modules/**/home.ts"],
+	{ eager: true },
+);
 
 /** 原始静态路由（未做任何处理） */
 const routes = [];
 
-Object.keys(modules).forEach((key) => {
-	routes.push(modules[key].default);
+// 将框架的路由 导入到需要处理的路由中
+// Object.keys(modules).forEach((key) => {
+// 	routes.push(modules[key].default);
+// });
+// 仅导入框架必须的路由
+Object.keys(routerModulesPureAdmin).forEach((key) => {
+	routes.push(routerModulesPureAdmin[key].default);
 });
+
+/** @ts-ignore 移除脏数据的 自动路由 */
+const cleanedAutoRoutes = disposalAutoRouter(autoRoutes);
+// 对自动路由进行排序
+sortRoutes(cleanedAutoRoutes);
+// 对自动路由进行重定向设置
+setRedirectByRank(cleanedAutoRoutes);
+/** 扁平化的 自动路由 用于测试 */
+const flattenAutoRoutesForTest = formatFlatteningRoutes(ascending(cleanedAutoRoutes.flat(Infinity)));
+cleanedAutoRoutes.forEach((route) => routes.push(route));
 
 /** 导出处理后的静态路由（三级及以上的路由全部拍成二级） */
 export const constantRoutes: Array<RouteRecordRaw> = formatTwoStageRoutes(
@@ -59,7 +85,7 @@ export const remainingPaths = Object.keys(remainingRouter).map((v) => {
 /** 创建路由实例 */
 export const router: Router = createRouter({
 	history: getHistoryMode(import.meta.env.VITE_ROUTER_HISTORY),
-	routes: constantRoutes.concat(...(setupLayouts(autoRoutes) as any), ...(remainingRouter as any)),
+	routes: setupLayouts([...constantRoutes, ...remainingRouter] as any),
 	strict: true,
 	scrollBehavior(to, from, savedPosition) {
 		return new Promise((resolve) => {
@@ -149,8 +175,45 @@ router.beforeEach((to: ToRouteType, _from, next) => {
 		} else {
 			// 刷新
 			if (usePermissionStoreHook().wholeMenus.length === 0 && to.path !== "/login") {
-				initRouter().then((router: Router) => {
+				function oldInitRouter() {
+					initRouter().then((router: Router) => {
+						if (!useMultiTagsStoreHook().getMultiTagsCache) {
+							const { path } = to;
+							const route = findRouteByPath(path, router.options.routes[0].children);
+							getTopMenu(true);
+							// query、params模式路由传参数的标签页不在此处处理
+							if (route && route.meta?.title) {
+								if (isAllEmpty(route.parentId) && route.meta?.backstage) {
+									// 此处为动态顶级路由（目录）
+									const { path, name, meta } = route.children[0];
+									useMultiTagsStoreHook().handleTags("push", {
+										path,
+										name,
+										meta,
+									});
+								} else {
+									const { path, name, meta } = route;
+									useMultiTagsStoreHook().handleTags("push", {
+										path,
+										name,
+										meta,
+									});
+								}
+							}
+						}
+						// 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
+						if (isAllEmpty(to.name)) router.push(to.fullPath);
+					});
+				}
+
+				/** @see https://pure-admin.cn/pages/routerMenu/#如何只要静态路由 */
+				function newInitRouter() {
+					// 使用下面方法替换initRouter
+					usePermissionStoreHook().handleWholeMenus([]);
+					addPathMatch();
 					if (!useMultiTagsStoreHook().getMultiTagsCache) {
+						console.log("newInitRouter to", to);
+
 						const { path } = to;
 						const route = findRouteByPath(path, router.options.routes[0].children);
 						getTopMenu(true);
@@ -174,9 +237,13 @@ router.beforeEach((to: ToRouteType, _from, next) => {
 							}
 						}
 					}
-					// 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
+					// 确保动态路由完全加入路由列表并且不影响静态路由
+					// （注意：动态路由刷新时router.beforeEach可能会触发两次，
+					// 第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，
+					// 如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
 					if (isAllEmpty(to.name)) router.push(to.fullPath);
-				});
+				}
+				newInitRouter();
 			}
 			toCorrectRoute();
 		}
