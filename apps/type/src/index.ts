efb5054f83015e9044ae3a7677523ce4d473c73e
ruan-cat <1219043956@qq.com>
2025-12-21 02:28:32 +0800

    🎈 perf(type): 避免单独导出代码的写法

--- apps/type/src/index.ts
+++ apps/type/src/index.ts
@@ -3,17 +3,9 @@
  * @description 本文件作为业务类型的统一导出入口
  */
 
-// 导出通用类型 - 先导出 common
+// 导出通用类型
 export * from "./common";
-
-// 导出业务类型 - 后导出 business，避免冲突时使用命名导出
-export { patrolMethodOptions, patrolPointStatusOptions, returnVisitStatusOptions } from "./common";
-
-// 选择性导出业务模块，避免重复导出
-export * from "./business/dev-team";
-export * from "./business/operation-team";
-export * from "./business/property-manage";
-export * from "./business/setting-manage";
-
+// 导出业务类型
+export * from "./business";
 // 导出常量
 export * from "./constant";
