f34c5fcf127c153ce4336fe65fba1b4950b5328e
ruan-cat <1219043956@qq.com>
2025-12-20 21:52:19 +0800

    🐞 fix(type): 修复代码编写故障。

--- apps/type/src/business/property-manage/patrol-manage/index.ts
+++ apps/type/src/business/property-manage/patrol-manage/index.ts
@@ -8,14 +8,4 @@ export * from "./item";
 export * from "./path";
 export * from "./plan";
 export * from "./point";
-/** 显式导出 task 模块，排除与 detail 模块冲突的 taskStatusOptions */
-export type {
-	PatrolTaskFormVO,
-	PatrolTaskFormProps,
-	TaskListItem,
-	TaskQueryParams,
-	PatrolTaskListItem,
-	PatrolTaskQueryParams,
-} from "./task";
-// 使用 export 导出值
-export { defaultPatrolTaskForm, patrolStatusOptions } from "./task";
+export * from "./task";
