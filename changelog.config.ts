97fca66173c77bf3f8d807bfba68285e52f65af4
ruan-cat <1219043956@qq.com>
2025-12-21 21:35:26 +0800

    🎈 perf(config): 精简写法

--- changelog.config.ts
+++ changelog.config.ts
@@ -1,7 +1,5 @@
 import type { ChangelogConfig } from "changelogen";
-
-import { commitTypes } from "@ruan-cat/commitlint-config/src/commit-types.ts";
-import { isConditionsSome } from "@ruan-cat/utils";
+import { changelogogenUseTypes } from "@ruan-cat/commitlint-config/src/changelogen-use-types.ts";
 
 /**
  * @see https://github.com/unjs/changelogen
@@ -10,25 +8,8 @@ export default {
 	// 提供明显的文件名
 	output: "CHANGELOG.md",
 
-	// TODO: changelogogenUseTypes in @ruan-cat/commitlint-config
-
 	/** @see https://github.com/viapip/ozon-tracker/blob/master/changelogen.config.json */
-	types: Object.fromEntries(
-		commitTypes
-			.map((commitType) => {
-				const { type, description, emoji } = commitType;
-				return [type, { title: `${emoji} ${description}` }];
-			})
-			.filter(([type]) =>
-				isConditionsSome([
-					() => type === "feat",
-					() => type === "fix",
-					() => type === "refactor",
-					() => type === "build",
-					() => type === "config",
-				]),
-			),
-	),
+	types: changelogogenUseTypes,
 
 	templates: {
 		commitMessage: "📢 publish: release package v{{newVersion}}",
