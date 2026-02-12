bc410e789d64e3976720b886b6e43062b2b6caf9
ruan-cat <1219043956@qq.com>
2026-02-13 00:51:55 +0800

    ✨ feat(type): 增加配置管理导出入口
    新增 dev-team/config-manage 统一导出入口，确保模块类型可被引用。

---
+++ apps/type/src/business/dev-team/config-manage/index.ts
@@ -0,0 +1,16 @@
+/**
+ * 开发团队-配置管理模块统一导出
+ * @description Dev team configuration management module exports
+ */
+
+// 字典管理
+export * from "./dictionary";
+
+// 配置项管理
+export * from "./item";
+
+// 配置类型管理
+export * from "./type";
+
+// 配置中心
+export * from "./center";
